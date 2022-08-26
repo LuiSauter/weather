79dfb1ed2420c207267883f8a41fa484947f7695
Luis Gabriel Janco <janco7249@gmail.com>
2022-08-26 09:04:28 -0400

    feat(Root): setup style with tailwindcss config

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App'
 import { ThemeProvider } from './context/ThemeProvider'
-import './index.css'
+import App from './App'
+import './styles/index.css'
 
 const root = createRoot(document.getElementById('root'))
 
