80663ad4fab4b960c5101538201ed627599b5e76
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-16 12:12:12 -0400

    test: rename describe to contain test

--- src/layout/header/header.test.jsx
+++ src/layout/header/header.test.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { describe, test, expect } from 'vitest'
-import Header from '.'
 import { render, screen } from '../../../__test__/utils/test-utils'
+import Header from '.'
 
-describe('Header', () => {
+describe('Component </Header>', () => {
   test('must display a title', () => {
     render(<Header />)
     expect(screen.getByText(/weather/i)).toBeInTheDocument()
