19f3891e825ffd12ac8ba2835af5f4507e1feed0
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-19 12:12:12 -0400

    refactor: rename 'home.jsx' to 'Home.jsx'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <section className='h-full relative max-w-7xl mx-auto'>
+    <section className='h-max w-full flex justify-center relative max-w-7xl mx-auto'>
       <h1>Home</h1>
     </section>
   )
