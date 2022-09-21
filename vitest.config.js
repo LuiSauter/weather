be533908c46308e528e81c7f693b54fad4f3b670
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-21 12:12:12 -0400

    chore: change vitest.config.js to read setup test.js

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom'
+    environment: 'jsdom',
+    setupFiles: [resolve(__dirname, '__test__/setup/setup.js')],
+    reporters: 'verbose'
   }
 })
