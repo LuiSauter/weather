24bef3e633b17a4f4264058d228b2c0c541b89e3
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-26 11:11:10 -0400

    chore: set vitePWA plugin to include logo and manifest.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import manifest from './manifest.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      manifest,
+      includeAssets: [
+        '/images/weather.svg'
+      ]
+    })
+  ]
 })
