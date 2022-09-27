842566cbca10c3e4c98316c6c3ac9f8915b34e49
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-27 11:11:10 -0400

    feat: set constants and endpoints to rapidapi of weather

---
+++ src/services/rapidapi.js
@@ -0,0 +1,14 @@
+// ForeCast weather API
+export const weatherUrl = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q='
+
+// Search/Autocomplete API:
+export const searchWeatherUrl = 'https://weatherapi-com.p.rapidapi.com/search.json?q='
+
+// Fetch Options:
+export const fetchOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': import.meta.env.VITE_RAPIDAPI_HOST,
+    'X-RapidAPI-Key': import.meta.env.VITE_RAPIDAPI_KEY
+  }
+}
