2731b5f13612dca52c808d9f62b3cc22e5ea5fce
Luis Gabriel Janco <janco7249@gmail.com>
2022-09-30 12:12:15 -0400

    feat: add SWR into a root app to data fetching

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeProvider'
 import App from './App'
+
+import { SWRConfig } from 'swr'
+import { fetcher } from './services/fetcher'
 import './styles/index.css'
 
 const root = createRoot(document.getElementById('root'))
@@ -11,7 +14,9 @@ root.render(
   <React.StrictMode>
     <ThemeProvider>
       <BrowserRouter>
-        <App />
+        <SWRConfig value={{ fetcher }}>
+          <App />
+        </SWRConfig>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
