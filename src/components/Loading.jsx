e49e55c7754a32ec6bcc5043ab8e56029751e886
Luis Gabriel Janco <janco7249@gmail.com>
2022-10-07 12:11:52 -0400

    refactor: add params to set size of the loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,7 @@
-const Loading = () => {
+const Loading = ({ size = 'h-5 w-5' }) => {
   return (
     <svg
-      className='animate-spin h-5 w-5 text-sky-500'
+      className={`animate-spin ${size} text-sky-500`}
       xmlns='http://www.w3.org/2000/svg'
       fill='none'
       viewBox='0 0 24 24'
