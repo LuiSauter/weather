edfa35f73a211d212f5bd9b8440b0186c4cf53cd
Luis Gabriel Janco <janco7249@gmail.com>
2022-10-07 12:11:52 -0400

    fix: remove tag <p> for <article>

--- src/components/resume/index.jsx
+++ src/components/resume/index.jsx
@@ -1,5 +1,7 @@
 import Card from './Card'
 import { FaTemperatureLow } from 'react-icons/fa'
+import { IoIosWater } from 'react-icons/io'
+import { FiWind } from 'react-icons/fi'
 
 const Resume = ({
   tempC,
@@ -11,26 +13,31 @@ const Resume = ({
   minTempC
 }) => {
   return (
-    <p>
-      <article className='flex flex-row flex-wrap justify-center px-4 py-8 gap-4'>
-        <Card
-          icon={iconCondition}
-          text={tempC}
-          textFooter={`°C - ${currentCondition}`}
-        />
-        <Card
-          icon={<FaTemperatureLow size={40} />}
-          isIcon
-          text={`${maxTempC} / ${minTempC}`}
-          textFooter='°C - Max / Min'
-        />
-        <Card
-          icon={iconCondition}
-          text={tempC}
-          textFooter={`°C - ${currentCondition}`}
-        />
-      </article>
-    </p>
+    <article className='flex flex-row flex-wrap justify-center px-4 pt-4 pb-4 gap-4 max-w-7xl'>
+      <Card
+        icon={iconCondition}
+        text={tempC}
+        textFooter={`°C - ${currentCondition}`}
+      />
+      <Card
+        icon={<FaTemperatureLow size={40} />}
+        isIcon
+        text={`${maxTempC} / ${minTempC}`}
+        textFooter='°C - Max / Min'
+      />
+      <Card
+        icon={<IoIosWater size={40} color='#5F9DF7' />}
+        text={humidity}
+        isIcon
+        textFooter='% - Humidity'
+      />
+      <Card
+        icon={<FiWind size={40} color='#7F8487' />}
+        text={windKph}
+        isIcon
+        textFooter='km/h - Wind speed'
+      />
+    </article>
   )
 }
 
