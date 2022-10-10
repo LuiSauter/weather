06696e5ab411893cbc20cf17ff121be40a00ba54
Luis Gabriel Janco <janco7249@gmail.com>
2022-10-10 11:10:24 -0400

    feat: add footer on the layout

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -6,6 +6,19 @@ const Layout = ({ children }) => {
     <main className='h-full bg-white dark:bg-gray-900 relative overflow-y-auto'>
       <Header />
       {children}
+      <footer className='flex w-full justify-center py-4 max-w-7xl mx-auto'>
+        <span className='font-light text-base text-slate-500'>
+          Built by{' '}
+          <a
+            href='https://sauterdev.vercel.app/'
+            target='_blank'
+            rel='noreferrer'
+            className='hover:underline'
+          >
+            Luis Gabriel Janco
+          </a>
+        </span>
+      </footer>
     </main>
   )
 }
