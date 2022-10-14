3fe35b960cd6caa2e48586e47fa1129045bbd6e0
Luis Gabriel Janco <janco7249@gmail.com>
2022-10-14 11:30:30 -0400

    test: add test to render footer

--- __test__/App.test.jsx
+++ __test__/App.test.jsx
@@ -3,9 +3,15 @@ import { BrowserRouter } from 'react-router-dom'
 import { render, screen } from './utils/test-utils'
 import App from '../src/App'
 
-describe('Simple working test', () => {
-  test('renders the first route that matches the URL', async () => {
+describe('Simple working App test', () => {
+  test('renders the first route that matches the URL', () => {
     render(<App />, { wrapper: BrowserRouter })
-    expect(screen.getByText(/weather/i)).toBeInTheDocument()
+    expect(screen.getByText('Weather')).toBeInTheDocument()
+  })
+
+  test('render footer', () => {
+    render(<App />, { wrapper: BrowserRouter })
+    screen.getByText('Built by')
+    screen.getByText('Luis Gabriel Janco')
   })
 })
