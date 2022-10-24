f3ce48b594a71fc8b85cecaf31b14fdbda464f7c
Luis Gabriel Janco <janco7249@gmail.com>
2022-10-24 11:22:33 -0400

    refactor: export as default all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,4 @@
-import { Toaster } from 'react-hot-toast'
-import { Routes, Route, useLocation } from 'react-router-dom'
-import Layout from './layout'
-import Home from './pages/Home'
-import Page404 from './pages/Page404'
-import Search from './pages/Search'
-
-function App () {
-  const location = useLocation()
-
-  return (
-    <>
-      <Layout>
-        <Routes key={location.pathname}>
-          <Route element={<Home />} path='/' />
-          <Route element={<Search />} path='/search/:name' />
-          <Route element={<Page404 />} path='/404' />
-          <Route element={<Page404 />} path='*' />
-        </Routes>
-      </Layout>
-      <Toaster position='bottom-center' reverseOrder={false} />
-    </>
-  )
-}
-
-export default App
+export { default as Layout } from './layout'
+export { default as Home } from './pages/Home'
+export { default as Page404 } from './pages/Page404'
+export { default as Search } from './pages/Search'
